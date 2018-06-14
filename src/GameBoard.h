2845eb7a6042ad3a5284f614374363773e6768ef
ArielKarp <akarpilovsky@gmail.com>
2018-06-15 00:00:57 +0300

    change to pointers

--- src/GameBoard.h
+++ src/GameBoard.h
@@ -13,6 +13,13 @@
 #include <memory>
 #include <tuple>
 
+//using std::make_unqiue;
+using std::unique_ptr;
+using std::make_pair;
+using std::pair;
+using std::cout;
+using std::endl;
+
 template<typename GAME_PIECE>
 using PieceInfo = std::unique_ptr<const std::pair<int, GAME_PIECE>>;
 
@@ -22,87 +29,138 @@ public:
 
 	GameBoard(): m_rows(ROWS), m_cols(COLS), m_player_num(PLAYER_NUM) {
 		// init board
+		m_board = new Elem**[m_rows];
+		for (int i = 0; i < m_rows; ++i) {
+			m_board[i] = new Elem*[m_cols];
+		}
 		for (int i = 0; i < m_rows; ++i) {
 			for (int j = 0; j < m_cols; ++j) {
-				m_board[i][j].first = -1;
+				m_board[i][j] = nullptr;
 			}
 		}
+
 	}
-	~GameBoard() = default;
+	~GameBoard() {
+		for (int i = 0; i < m_rows; ++i) {
+			delete[] m_board[i];
+		}
+		delete[] m_board;
+	}
 
 	GameBoard operator=(const GameBoard&) = delete;
 
 	GameBoard(const GameBoard&) = delete;
 
-	void print() { std::cout << m_rows << " " << m_cols << " " << m_player_num << std::endl; }
-
+	void print() { }
 	// getPiece
 	PieceInfo<GAME_PIECE> getPiece(int row, int col) {
-		if (m_board[row][col].first == -1) {
+		if (m_board[row][col] == nullptr) {
 			return nullptr;
 		}
-		return std::make_unique<const std::pair<int, GAME_PIECE>>
-				(std::make_pair(m_board[row][col].first, m_board[row][col].second));
+		return std::make_unique<const pair<int, GAME_PIECE>>(make_pair(m_board[row][col]->getPlayerNum(), m_board[row][col]->getPiece()));
 	}
 
 	// setPiece
 	PieceInfo<GAME_PIECE> setPiece(int row, int col, GAME_PIECE piece, int player) {
 		PieceInfo<GAME_PIECE> l_prevPiece = getPiece(row, col);
-		m_board[row][col] = std::make_pair(player, piece);
+		if (m_board[row][col] == nullptr) {
+			// new piece
+			m_board[row][col] = new Elem(player, piece);
+		} else {
+			m_board[row][col]->setPlayerNum(player);
+			m_board[row][col]->setGamePiece(piece);
+		}
 		return l_prevPiece;
 	}
 
+	class Elem{
+	public:
+		Elem(int a_playerNum, const GAME_PIECE& a_piece) : m_playerNum(a_playerNum), m_piece(a_piece) {}
+		int getPlayerNum() const { return m_playerNum; }
+		GAME_PIECE getPiece() const { return m_piece; }
+		void setPlayerNum(int a_playerNum) { m_playerNum = a_playerNum; }
+		void setGamePiece(GAME_PIECE a_gamePiece) {m_piece = a_gamePiece; }
+	private:
+		int m_playerNum;
+		GAME_PIECE m_piece;
+	};
+
 	class GeneralIter {
 	public:
-		GeneralIter(int a_rows, int a_cols, int a_row, int a_col, std::pair<int, GAME_PIECE>* a_iter_pos = nullptr):
-			m_rows(a_rows), m_cols(a_cols), m_row(a_row), m_col(a_col), m_iter_pos(a_iter_pos) {}
-		const std::tuple<int, int, GAME_PIECE, int> operator*() {
-			return std::tuple<int, int, GAME_PIECE, int>(m_row, m_col, m_iter_pos->second, m_iter_pos->first);
+		GeneralIter(int a_rows, int a_cols, int a_row, int a_col, Elem*** a_boardHandle = nullptr):
+		m_rows(a_rows), m_cols(a_cols), m_row(a_row), m_col(a_col), m_boardHandle(a_boardHandle){
 		}
-		GeneralIter operator++() {
+
+		const std::tuple<int, int, GAME_PIECE, int> operator*() {
+			return std::tuple<int, int, GAME_PIECE, int>(m_row, m_col, m_boardHandle[m_row][m_col]->getPiece(), m_boardHandle[m_row][m_col]->getPlayerNum());
+		}
+		GeneralIter& operator++() {
+
 			// get to end of row
-			std::pair<int, GAME_PIECE>* l_temp = m_iter_pos + 1;
-			for (int i = m_col + 1; i < m_cols; ++i, ++l_temp) {
-				if (l_temp->first != -1) {
-					m_iter_pos = l_temp;
-					return GeneralIter(m_rows, m_cols, m_row, i, ++m_iter_pos);
+			for (int j = (m_col + 1); j < m_cols; ++j) {
+				if (m_boardHandle[m_row][j] != nullptr) {
+					cout << "Inside location : i " << m_row << " j :" << j << endl;
+					m_col = j;
+					return *this;
 				}
 			}
-			for (int i = m_row + 1; i < m_rows; ++i) {
-				for (int j = 0; j < m_cols; ++j, ++l_temp) {
-					if (l_temp->first != -1) {
-						m_iter_pos = l_temp;
-						return GeneralIter(m_rows, m_cols, i, j, m_iter_pos);
+
+			// go over the board
+			for (int i = (m_row + 1); i < m_rows; ++i) {
+				for (int j = 0; j < m_cols; ++j) {
+					if (m_boardHandle[i][j] != nullptr) {
+						m_row = i;
+						m_col = i;
+						return *this;
 					}
 				}
 			}
-			m_iter_pos = l_temp;
-			return GeneralIter(m_rows, m_cols, m_rows, m_rows, m_iter_pos);
+			// in case not on board, set to end
+			m_row++;
+			m_col++;
+			return *this;
 		}
 		bool operator!= (GeneralIter a_otherGenIter) {
-			return m_iter_pos != a_otherGenIter.m_iter_pos;
+			if (m_row != a_otherGenIter.m_row || m_col != a_otherGenIter.m_col) {
+				return true;
+			}
+			return false;
+			//return m_pos != a_otherGenIter.m_pos;
 		}
 	private:
 		int m_rows;
 		int m_cols;
 		int m_row;
 		int m_col;
-		std::pair<int, GAME_PIECE>* m_iter_pos;
+		Elem*** m_boardHandle;
 	};
 
 	GeneralIter begin() {
-		return GeneralIter(m_rows, m_cols, 0, 0, &m_board[0][0]);
+		// search for starting pos
+		int l_row = 0;
+		int l_col = 0;
+		int flag = 0;
+		for (int i = 0; i < m_rows && !flag; ++i) {
+			for (int j = 0; j < m_cols; ++j) {
+				if (m_board[i][j] != nullptr) {
+					l_row = i;
+					l_col = j;
+					flag = 1;
+					break;
+				}
+			}
+		}
+		return GeneralIter(m_rows, m_cols, l_row, l_col, m_board);
 	}
 	GeneralIter end() {
-		return GeneralIter(m_rows, m_cols, -1, -1, &m_board[m_rows-1][m_cols-1] + 1);
+		return GeneralIter(m_rows, m_cols, m_rows, m_cols);
 	}
 
 private:
+	Elem*** m_board;
 	int m_rows;
 	int m_cols;
 	int m_player_num;
-	//std::vector<std::vector<std::shared_ptr<std::pair<int, GAME_PIECE>>>> m_board;
-	std::pair<int, GAME_PIECE> m_board[ROWS][COLS];
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,32 @@
 
 #include "GameBoard.h"
 
+
+class A{
+public:
+	A() = delete;
+	A(int i) {
+		std::cout << "not in default" << std::endl;
+	}
+};
+
+
 int main(int argc, char* argv[]) {
 	GameBoard<10, 10, char> board1;
 	board1.print();
-	board1.setPiece(0, 0, '0', 0);
-	board1.setPiece(1, 1, '1', 0);
-	board1.setPiece(2, 2, '2', 0);
-	for (auto& l_piece : board1) {
-		std::cout << std::get<2>(l_piece) << std::endl;
+	board1.setPiece(0, 0, '4', 0);
+	board1.setPiece(1, 1, '0', 0);
+	board1.setPiece(2, 2, '1', 0);
+	board1.setPiece(3, 3, '2', 0);
+	//for (auto& l_piece : board1) {
+	//	std::cout << std::get<2>(l_piece) << std::endl;
+	//}
+//	GameBoard<10, 10, A> board2;
+	GameBoard<10, 10, char>::GeneralIter iter = board1.begin();
+	for (; iter != board1.end(); ++iter) {
+		std::cout << std::get<2>(*iter) << std::endl;
+		//exit(1);
 	}
-//	GameBoard<10, 10, char>::GeneralIter iter = board1.begin();
-//	for (; iter != board1.end(); ++iter) {
-//		std::cout << std::get<2>(*iter) << std::endl;
-//	}
 	return 0;
 }
 
